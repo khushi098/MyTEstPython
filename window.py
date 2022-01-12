048cc7bb9b809899720461c07a571f10e3993dc7
Khushi Jain <khushi098jain@gmail.com>
2022-01-12 21:59:57 +0530

    k

--- window.py
+++ window.py
@@ -3,17 +3,22 @@ from tkinter import *
 from tkinter import ttk
 from PIL import Image, ImageTk
 
-
-
 # 1. Create the Main window
 
 win = tk.Tk() # Tk is a class of Tkinter
-win.title('                                                                                                                                                                                                     \t \t\t\t\t\t\t\tVisual Studio Code')
+Title = win.title('                                                                                                                                                                                                    \t \t\t\t\t\t\t\tVisual Studio Code')
+
+'''
+win.overrideredirect(True)
+#win.title_bar=ttk.Combobox(values=["Mode 1","Mode 2"])
+#win.title_bar.set("Mode 1")
+#win.title.state(["readonly"])
+#win.title.pack() '''
+
 win.state('zoomed')
 #win.geometry('400x200') #size of window
-style = ttk.Style() # style for sash and handle 
-style.theme_use('classic')
-
+#style = ttk.Style() # style for sash and handle 
+#style.theme_use('classic')
 
 # for scroll bar 
 
@@ -30,21 +35,30 @@ left_list = tk.Listbox(win)
 left_list.pack(side=tk.LEFT)
 pw.add(left_list )
 left_list.config(bg = "lightcyan4")
-
+'''
 right_list = tk.Listbox(win)
 right_list.pack(side=tk.LEFT)
 pw.add(right_list)
-right_list.config(bg = "lightpink")
+right_list.config(bg = "lightpink") '''
 
 pw.pack(fill=tk.BOTH,expand=True)
 
+#for creating text widget
+text =Text(pw)
+text.pack()
+text.config(bg ="cyan")
+pw.add(text)
+
 #for icon
-try: #set icon for window
+try: #set icon for window 
   win.iconbitmap('vsicon.ico')
 except:
     pass
 
-menubar = Menu()     # Menu is a Class of Tkinter Library
+menubar = Menu()# Menu is a Class of Tkinter Library
+
+#Title =win.title(f' {}                                                                                                                                                                                                     \t \t\t\t\t\t\t\tVisual Studio Code')
+
 file = Menu(menubar, tearoff = False)
 edit = Menu(menubar, tearoff = False)
 selection = Menu(menubar, tearoff = False)
@@ -53,20 +67,21 @@ go = Menu(menubar, tearoff = False)
 run = Menu(menubar, tearoff = False)
 terminal = Menu(menubar, tearoff = False)
 help = Menu(menubar, tearoff = False)
+
 '''
 #for toolbar
 toolbar = Frame (win.master, bd= 2, relief=RAISED)
-
+"""
 win.img = Image.open("p.png")
 eimg = ImageTk.PhotoImage(win.img)
 
 exitButton = Button(toolbar, image=eimg, relief=FLAT, command=win.quit)
 exitButton.image = eimg
-exitButton.pack(side=LEFT, padx=2, pady=2)
+exitButton.pack(side=LEFT, padx=2, pady=2)  """
 toolbar.pack(side=LEFT, fill=Y)
-#toolbar.config(height = 30)
-#toolbar.pack() '''
-
+#toolbar.config(height = 3,bg = 'red')
+#pw.add(toolbar) 
+'''
 #create menu
 menubar.add_cascade(label='File',menu =file)
 menubar.add_cascade(label='Edit',menu =edit)
@@ -101,14 +116,26 @@ file.add_command(label="Auto Save")
  
  #add submenu to preference
 sub_menu = Menu(file)
-sub_menu.add_command(label="color theme",accelerator="ctrl+k+ctrl+t")
+sub_menu.add_command(label="setting",accelerator="ctrl+,")
+sub_menu.add_command(label="online services setting")
+sub_menu.add_command(label="telemetry setting")
+sub_menu.add_command(label="Extensions",accelerator="ctrl+shift+x")
 sub_menu.add_separator()
-sub_menu.add_command(label="setting",accelerator="ctrl+st")
-sub_menu.add_command(label="extension",accelerator="ctrl+x+ctrl+e") 
+sub_menu.add_command(label="keyboard shortcuts",accelerator="ctrl+k+ctrl+s")
+sub_menu.add_command(label="Migrate Keyboard shortcuts from...")
+sub_menu.add_separator()
+sub_menu.add_command(label="User Snippets")
+sub_menu.add_separator()
+sub_menu.add_command(label="color Theme",accelerator="ctrl+k+ctrl+t")
+sub_menu.add_command(label="File Icon Theme")
+sub_menu.add_command(label="Product icon Theme")
+sub_menu.add_separator()
+sub_menu.add_command(label="Turn On Settings Sync...") 
 file.add_command(label="Preferences")
 file.add_cascade(label="Preferances",menu=sub_menu)
 
 sub_menu.config(bg="cyan",font=12)
+
 file.add_separator()
 file.add_command(label="Revert File")
 file.add_command(label="Close Editor", accelerator="ctrl+F4")
@@ -120,6 +147,8 @@ file.add_command(label="Exit File",command=exit, accelerator="altr+X")
 #to set file menu theme
 file.config(bg="pink",fg="black",activebackground="blue",activeforeground="red",activeborderwidth=2)
 file.config(font="calbri")
+
+# to add edit menu
 
 win.config(menu = menubar)
 
